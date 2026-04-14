0ec55e81cd4f8eaae802c999ea0c9429e515e539
Raul Matthew Mosley <raulmatthewmosley@gmail.com>
2026-04-14 19:44:12 -0400

    exercise 1 done

--- apputil.py
+++ apputil.py
@@ -3,13 +3,23 @@ from sklearn.cluster import KMeans
 
 
 def kmeans(X, k):
+    """ Run the scikit learn kmeans model on the data X and return
+    the results.
+
+    Args:
+        X (np.array): the data to cluster
+        k (int): the number of clusters
+
+    Returns:
+        tuple: a np.array of the centroids followed by an np.array of
+        the cluster indices for each point in X
+    """
     # Create the KMeans model
     km = KMeans(n_clusters=k)
     km.fit(X)
 
-    centroids = km.cluster_centers_
-
-    return centroids
+    # Return the centroids and the centroid labels corresponding to each point
+    return km.cluster_centers_, km.labels_
 
 
 
