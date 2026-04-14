b7003aa72c4d21ceff0cf0dc1002ae8c08c4fe5a
Raul Matthew Mosley <raulmatthewmosley@gmail.com>
2026-04-14 19:38:45 -0400

    kmeans gets centroids

--- apputil.py
+++ apputil.py
@@ -1,1 +1,26 @@
-# your code here
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+def kmeans(X, k):
+    # Create the KMeans model
+    km = KMeans(n_clusters=k)
+    km.fit(X)
+
+    centroids = km.cluster_centers_
+
+    return centroids
+
+
+
+if __name__ == '__main__':
+    import random
+
+    # Testing
+    X = []
+    for i in range(10):
+        X.append([random.randint(0, 100), random.randint(0, 100)])
+
+    X = np.array(X)
+
+    print(kmeans(X, 3))
